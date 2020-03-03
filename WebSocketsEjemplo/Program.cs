5ec639192a117054f365365612f5545c3632c49a
JulianPL99 <55467386+JulianPL99@users.noreply.github.com>
2020-03-02 21:16:48 -0500

    Missing files

---
+++ WebSocketsEjemplo/Program.cs
@@ -0,0 +1,49 @@
+﻿using SuperSocket.SocketBase;
+using SuperWebSocket;
+using System;
+
+namespace WebSocketsEjemplo
+{
+    class Program
+    {
+        public static WebSocketServer wsServer;
+        static void Main(string[] args)
+        {
+            wsServer = new WebSocketServer();
+            int port = 8088;
+            wsServer.Setup(port);
+            wsServer.NewSessionConnected += WsServer_NewSessionConnected;
+            wsServer.NewMessageReceived += WsServer_NewMessageReceived;
+            wsServer.NewDataReceived += WsServer_NewDataReceived;
+            wsServer.SessionClosed += WsServer_SessionClosed;
+            wsServer.Start();
+            Console.WriteLine("Server is running on port " + port + ". Press ENTER to exit.");
+            Console.ReadKey();
+            wsServer.Stop();
+        }
+
+        private static void WsServer_SessionClosed(WebSocketSession session, CloseReason value)
+        {
+            Console.WriteLine("SessionClosed");
+        }
+
+        private static void WsServer_NewDataReceived(WebSocketSession session, byte[] value)
+        {
+            Console.WriteLine("NewDataReceived");
+        }
+
+        private static void WsServer_NewMessageReceived(WebSocketSession session, string value)
+        {
+            Console.WriteLine("NewMessageReceived: " + value);
+            if (value == "Hello server")
+            {
+                session.Send("Hello client");
+            }
+        }
+
+        private static void WsServer_NewSessionConnected(WebSocketSession session)
+        {
+            Console.WriteLine("NewSessionConnected");
+        }
+    }
+}
